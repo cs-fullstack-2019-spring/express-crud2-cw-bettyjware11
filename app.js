579e1ad01649011851f10e2243b946fecf8ccb2a
Kenn <kenn+git@code-crew.org>
2019-03-27 13:42:58 -0500

    Graded 03-27-2019 13:42:42

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ var fillPostData = require('./routes/fillPostData');
 
 var app = express();
 
-//Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://admin:admin@cluster0-higgl.mongodb.net/mongoTest?retryWrites=true';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// //Set up mongoose connection
+// var mongoose = require('mongoose');
+// var mongoDB = 'mongodb+srv://admin:admin@cluster0-higgl.mongodb.net/mongoTest?retryWrites=true';
+// mongoose.connect(mongoDB, { useNewUrlParser: true });
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
